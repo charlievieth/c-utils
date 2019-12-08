de2a1a702ff31a36565237227d9db6ca4f4147b5
Charlie Vieth <cvieth@lyft.com>
2019-12-07 20:20:11 -0500

    glob: initial commit

---
+++ glob/glob.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <glob.h>
+
+
+int main(int argc, char const *argv[]) {
+	if (argc <= 1) {
+		fprintf(stderr, "error: missing pattern\n");
+		return 1;
+	}
+
+	// TODO:
+	//  1. print per-iteration
+	//  2. error func
+
+	int flags =  GLOB_MARK | GLOB_TILDE | GLOB_BRACE;
+	glob_t g = { 0 };
+	for (int i = 1; i < argc; i++) {
+		if (i == 2) {
+			flags |= GLOB_APPEND;
+		}
+		glob(argv[i], flags, NULL, &g);
+	}
+	for (size_t i = 0; i < g.gl_pathc; i++) {
+		printf("%s\n", g.gl_pathv[i]);
+	}
+	globfree(&g);
+	return 0;
+}
