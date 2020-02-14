87c298fa8a02a015e7333e5cef4464c39fbdee67
Charlie Vieth <cvieth@lyft.com>
2020-02-13 21:39:15 -0500

    zero: WIP: IDK a better /dev/zero ???

---
+++ zero/null.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+#define likely(x)   __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
+#define BUFSIZE (1024 * 1024)
+
+int main() {
+	char buf[BUFSIZE] __attribute__((aligned(4096))); // page align
+	char out[BUFSIZE] __attribute__((aligned(4096)));
+
+	memset(buf, 0, BUFSIZE);
+	if (setvbuf(stdin, out, _IOFBF, 1024 * 1024) != 0) {
+		fprintf(stderr, "setvbuf failed\n");
+		return 1;
+	}
+	while (1) {
+		if (unlikely(fread(buf, 1, BUFSIZE, stdin) < BUFSIZE)) {
+			break;
+		}
+	}
+	return 0;
+}
