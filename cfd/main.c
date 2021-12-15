85e820924930e0800d91caee79279178803d2ab5
Charlie Vieth <charlie.vieth@gmail.com>
2021-12-15 17:57:56 -0500

    cfd: add usage, support NUL delims and fix a bug

--- cfd/main.c
+++ cfd/main.c
@@ -4,9 +4,11 @@
 #include <limits.h>
 #include <errno.h>
 #include <unistd.h>
-
+#include <stdbool.h>
 #include <assert.h>
 
+// TODO: consider using: getprogname()
+#define PROGRAM_NAME "cfd"
 
 #ifndef unlikely
 #define unlikely(x) __builtin_expect(!!(x), 0)
@@ -89,40 +91,37 @@ static_assert(strlen(slash_prefix) == prefix_len, "slash_prefix");
 static_assert(strlen(ansii_prefix) == prefix_len, "ansii_prefix");
 
 static ssize_t trim_prefix(char **dst, size_t *dst_cap, const char *buf, const size_t buf_len) {
+	if (unlikely(expand_buffer(dst, dst_cap, buf_len) != 0)) {
+		return -1;
+	}
+	char *d = *dst;
 	if (buf_len > prefix_len) {
-		if (unlikely(expand_buffer(dst, dst_cap, buf_len - prefix_len) != 0)) {
-			return -1;
-		}
 		if (memcmp(buf, slash_prefix, prefix_len) == 0) {
 			size_t n = buf_len - prefix_len;
-			memcpy(*dst, &buf[prefix_len], n);
+			memcpy(d, &buf[prefix_len], n);
 			return n;
 		}
+		// TODO: remove and write ANSII prefix then use the
+		// above logic to strip the prefix.
 		if (memcmp(buf, ansii_prefix, prefix_len) == 0) {
 			const char *p = memchr(buf, 'm', buf_len);
 			if (p) {
 				ssize_t i = p - buf;
-				char *d = *dst;
 				memcpy(d, buf, i + 1);
 				memcpy(&d[i + 1], &buf[i + prefix_len + 1], buf_len - i - prefix_len);
 				return buf_len - prefix_len;;
 			}
 		}
 	}
-	memcpy(dst, buf, buf_len);
+	memcpy(d, buf, buf_len);
 	return buf_len;
 }
 
-int main(int argc, char const *argv[]) {
-	(void)argc;
-	(void)argv;
-
-	const int delim = '\n';
-
-	size_t buf_cap = 512;
-	size_t dst_cap = 512;
-	char *buf = xmalloc(512);
-	char *dst = xmalloc(512);
+static int consume_stdin(const unsigned char delim) {
+	size_t buf_cap = 256;
+	size_t dst_cap = 256;
+	char *buf = xmalloc(256);
+	char *dst = xmalloc(256);
 
 	ssize_t i;
 	while ((i = getdelim(&buf, &buf_cap, delim, stdin)) != -1) {
@@ -149,4 +148,54 @@ int main(int argc, char const *argv[]) {
 fatal_error:
 	fprintf(stderr, "fatal error");
 	return 1;
+}
+
+static void print_usage(bool print_error) {
+	if (print_error) {
+		fprintf(stderr, "Usage: %s [OPTION]...\n", PROGRAM_NAME);
+		fprintf(stderr, "Try '%s --help' for more information.\n", PROGRAM_NAME);
+	} else {
+		printf("Usage: %s [OPTION]... [FILE]...\n", PROGRAM_NAME);
+		fputs("\n\
+Strip './' filename prefixes from the output of fd (fd-find).\n\
+\n\
+Options:\n\
+  -0, --print0  Line delimiter is NUL, not newline\n\
+  -h, --help    Print this help message and exit.\n", stdout);
+	}
+}
+
+int main(int argc, char const *argv[]) {
+	bool null_terminate = false;
+	bool invalid_flag = false;
+	bool print_help = false;
+
+	for (int i = 1; i < argc; i++) {
+		if (strcmp(argv[i], "-0") == 0 || strcmp(argv[i], "--print0") == 0) {
+			if (null_terminate) {
+				fprintf(stderr,"%s: null terminate flag ['-0', '--print0'] specified twice\n",
+					PROGRAM_NAME);
+				invalid_flag = true;
+			}
+			null_terminate = true;
+		} else if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+			print_help = true;
+			break;
+		} else {
+			fprintf(stderr, "%s: unrecognized option: '%s'\n", PROGRAM_NAME, argv[i]);
+			invalid_flag = true;
+		}
+	}
+	if (invalid_flag) {
+		print_usage(true);
+		return 2;
+	}
+	if (print_help) {
+		print_usage(false);
+		return 0;
+	}
+
+	const unsigned char delim = null_terminate ? 0 : '\n';
+
+	return consume_stdin(delim);
 }
